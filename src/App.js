f3fe289232fd72d4f0a9ba023278a5dd8cb29050
Kiran88 <62086331+riazk88@users.noreply.github.com>
2020-11-02 18:42:20 -0700

    added search box and API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+
+  const APP_ID='14ed0ad9';
+  const APP_KEY="ebb1f6f6e4aa0f8e89227b170bd47a1e"; 
+
+  const exampleReq=`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+  return(
     <div className="App">
-      <h1>Hello Foodie!</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
