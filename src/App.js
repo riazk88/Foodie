76d1b828c81683f0f9c0e71256be545acf783b11
Kiran88 <62086331+riazk88@users.noreply.github.com>
2020-11-07 18:32:38 -0700

    added to app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 
 const App = () => {
@@ -6,7 +6,20 @@ const App = () => {
   const APP_ID='14ed0ad9';
   const APP_KEY="ebb1f6f6e4aa0f8e89227b170bd47a1e"; 
 
-  const exampleReq=`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch (
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
 
   return(
     <div className="App">
